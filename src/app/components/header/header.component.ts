13af75bef8cd312d696942340c675816df582e8c
Aurore S <ausalavert@gmail.com>
2023-07-30 16:31:43 +0200

    Header changes between home and project page

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        const currentUrl = event.url;
+
+        const isProjectDetailPage = currentUrl.includes('/project');
+
+        if (isProjectDetailPage) {
+          document.querySelector('header').classList.add('header-alt');
+        } else {
+          document.querySelector('header').classList.remove('header-alt');
+        }
+      }
+    })
   }
 
 }
