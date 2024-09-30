9ac1bc80f73f8ec4c1a66431dc2286f1cc285e78
aurore <aurore.salavert@freecaster.com>
2024-09-30 20:15:35 +0200

    Header scroll

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+  constructor(private renderer: Renderer2, private el: ElementRef) { }
+  
+  scrolled = false;
 
-  constructor() { }
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollPosition = window.pageYOffset;
+    const triggerHeight = window.innerHeight * 0.75;
 
-  ngOnInit(): void {
+    if (scrollPosition > triggerHeight) {
+      this.renderer.addClass(this.el.nativeElement, 'scrolled');
+    } else {
+      this.renderer.removeClass(this.el.nativeElement, 'scrolled');
+    }
   }
-
 }
