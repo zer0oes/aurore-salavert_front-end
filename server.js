699bc01beb87968fd1efb7b3735443da354c8f06
aurore <aurore.salavert@freecaster.com>
2024-10-08 09:14:40 +0200

    Add redirect for www to non-www

--- server.js
+++ server.js
@@ -2,21 +2,22 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve only the static files from the dist directory
-app.use(express.static(__dirname + '/dist/aurore-salavert_front-end'));
-
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+'/dist/aurore-salavert_front-end/index.html'));
-});
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
-
-
 app.use((req, res, next) => {
     if (req.headers.host.startsWith('www.')) {
-        const newHost = req.headers.host.slice(4); // Retire "www."
+        const newHost = req.headers.host.slice(4);
         return res.redirect(301, `https://${newHost}${req.originalUrl}`);
     }
     next();
+});
+
+// Serve only the static files from the dist directory
+app.use(express.static(path.join(__dirname, '/dist/aurore-salavert_front-end')));
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, '/dist/aurore-salavert_front-end/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080, () => {
+    console.log(`Server is running on port ${process.env.PORT || 8080}`);
 });
