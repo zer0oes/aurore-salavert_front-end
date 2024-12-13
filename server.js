f90e850001576d3c8ff6bcc8631bc6f32c6a41d6
aurore <aurore.salavert@freecaster.com>
2024-12-13 15:10:46 +0100

    Fixed: Redirect http to https

--- server.js
+++ server.js
@@ -3,13 +3,11 @@ const path = require('path');
 const app = express();
 
 app.use((req, res, next) => {
-    if (req.headers.host === 'aurore-salavert.fr') {
-        return res.redirect(301, `https://www.aurore-salavert.fr${req.originalUrl}`);
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+        return res.redirect(301, `https://${req.headers.host}${req.originalUrl}`);
     }
     next();
 });
-
-app.use(express.static(path.join(__dirname, '/dist/aurore-salavert_front-end')));
 
 app.get('/robots.txt', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist/aurore-salavert_front-end/robots.txt'));
@@ -19,6 +17,7 @@ app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname, '/dist/aurore-salavert_front-end/index.html'));
 });
 
-app.listen(process.env.PORT || 8080, () => {
-    console.log(`Server is running on port ${process.env.PORT || 8080}`);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
